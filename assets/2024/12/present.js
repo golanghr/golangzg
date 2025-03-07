935a4d6a09157ff08b9fc35e0f8e0695b7c36d6b
Zlatko Bratkovic <zlatko.bratkovic@gmail.com>
2025-03-07 23:42:53 +0100

    Add GoTalks 2024-12

---
+++ assets/2024/12/present.js
@@ -0,0 +1,20 @@
+(function() {
+    var beforePrint = function() {
+    };
+    var afterPrint = function() {
+    };
+
+    if (window.matchMedia) {
+        var mediaQueryList = window.matchMedia('print');
+        mediaQueryList.addListener(function(mql) {
+            if (mql.matches) {
+                beforePrint();
+            } else {
+                afterPrint();
+            }
+        });
+    }
+
+    window.onbeforeprint = beforePrint;
+    window.onafterprint = afterPrint;
+}());
